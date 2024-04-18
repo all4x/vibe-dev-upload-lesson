ffc00ceb956a217b399dd1720cbe53a490ffcd55
Alex Rodrigues <54729143+all4x@users.noreply.github.com>
2024-04-18 02:21:18 -0300

    Update actions.ts with zod validate
    import { z } from "zod";
    
    const MAX_UPLOAD_SIZE = 1024 * 1024 * 20; // 5MB
    const ACCEPTED_IMAGE_MIME_TYPES = ["image/jpeg", "image/jpg", "image/png"];
    
    export const schemaUploadFormZod = z.object({
      file: z
        .instanceof(File)
        .refine((files) => {
          return files.size <= MAX_UPLOAD_SIZE;
        }, `Tamanho máximo da imagem é 5MB.`)
        .refine(
          (files) => ACCEPTED_IMAGE_MIME_TYPES.includes(files.type),
          "Apenas formatos .PNG .JPEG .JPG são suportados.",
        ),
    });

--- src/app/(main)/actions.ts
+++ src/app/(main)/actions.ts
@@ -1,12 +1,28 @@
-'use server'
+"use server";
 
-import { storageProvider } from "@/services/storage"
+import { storageProvider } from "@/services/storage";
+import { schemaUploadFormZod } from "./schema";
+import { z } from "zod";
 
 export async function submitFormAction(prevState: any, formData: FormData) {
-  const file = formData.get('file') as File
-  const url = await storageProvider.upload(file)
-  
-  return {
-    url
+  const img = formData.get("file") as File;
+
+  try {
+    const fileParse = schemaUploadFormZod.parse({ file: img });
+    const { file } = fileParse;
+
+    const url = await storageProvider.upload(file);
+
+    return {
+      url,
+      error: "",
+    };
+  } catch (error) {
+    if (error instanceof z.ZodError) {
+      return {
+        url: "",
+        error: error.errors.map((err) => err.message),
+      };
+    }
   }
 }
